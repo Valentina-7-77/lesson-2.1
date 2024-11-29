eab335f3f3ae2c64df39b6cfe0195466ef2b5234
Valentina-7-77 <natandr777@gmail.com>
2024-11-29 23:39:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Variables Переменные
+count_of_home_tasks = 12
+count_of_spent_time = 1.5
+name_of_curse = 'Python'
+time_for_one_task = count_of_spent_time / count_of_home_tasks
+
+print('Курс:', name_of_curse + ',', 'всего задач:' , str(count_of_home_tasks) + ',', 'затрачено времени:', str(count_of_spent_time) + ',', 'среднее время выполнения:', time_for_one_task)
